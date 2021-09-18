aa187abdb40601fd0186562182e44f97e2fd238d
Zhamshid Irisbayev <180103263@stu.sdu.edu.kz>
2021-09-19 00:24:57 +0600

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Hello',
+          style: TextStyle(
+            letterSpacing: 3,
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: HomePage(),
+    )
   ));
 }
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
+
 
