da00dee73b29d2d7e7f766d8422120c46196ade6
Zhamshid Irisbayev <180103263@stu.sdu.edu.kz>
2021-09-19 22:41:49 +0600

    adding audofiles and plugins to play

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    body: SafeArea(
-      child: Center(
-        child: Text('Hi'),
+  runApp(XyloPhone());
+}
+
+class XyloPhone extends StatelessWidget {
+  const XyloPhone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: FlatButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('assets_note1.wav');
+            },
+            child: Text('Click me'),
+          ),
+        ),
       ),
-    ),
-  )));
+    ));
+  }
 }
