984a9951c1fc1e665c859b5e016ef6f2fd4aad18
Zhamshid Irisbayev <180103263@stu.sdu.edu.kz>
2021-09-22 18:24:56 +0600

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(XyloPhone());
-}
+void main() => runApp(XyloPhone());
 
 class XyloPhone extends StatelessWidget {
   void playSound(int soundNumber) {
@@ -12,13 +10,13 @@ class XyloPhone extends StatelessWidget {
     player.play('assets_note$soundNumber.wav');
   }
 
-  Expanded buildKey({required Color color, required int soundNumber,required String name}) {
+  Expanded buildKey(
+      {required Color color, required int soundNumber, required String name}) {
     return Expanded(
       child: FlatButton(
         color: color,
         onPressed: () {
           playSound(soundNumber);
-
         },
         child: Text(
           name,
@@ -42,13 +40,13 @@ class XyloPhone extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.red,soundNumber: 1,name: 'Note 1'),
-              buildKey(color: Colors.orange,soundNumber: 2,name: 'Note 2'),
-              buildKey(color: Colors.yellow,soundNumber: 3,name: 'Note 3'),
-              buildKey(color: Colors.green,soundNumber: 4,name: 'Note 4'),
-              buildKey(color: Colors.teal,soundNumber: 5,name: 'Note 5'),
-              buildKey(color: Colors.blue,soundNumber: 6,name: 'Note 6'),
-              buildKey(color: Colors.purple,soundNumber: 7,name: 'Note 7'),
+              buildKey(color: Colors.red, soundNumber: 1, name: 'Note 1'),
+              buildKey(color: Colors.orange, soundNumber: 2, name: 'Note 2'),
+              buildKey(color: Colors.yellow, soundNumber: 3, name: 'Note 3'),
+              buildKey(color: Colors.green, soundNumber: 4, name: 'Note 4'),
+              buildKey(color: Colors.teal, soundNumber: 5, name: 'Note 5'),
+              buildKey(color: Colors.blue, soundNumber: 6, name: 'Note 6'),
+              buildKey(color: Colors.purple, soundNumber: 7, name: 'Note 7'),
             ],
           ),
         ),
